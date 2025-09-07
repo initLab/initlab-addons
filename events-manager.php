ff97ffe4e1ce586d218f71685f46cb3a5bf32298
Vencislav Atanasov <git@6bez10.info>
2025-09-07 15:14:21 +0300

    v2.6: Add a new Events manager scope to show future events without the "private" tag set

--- events-manager.php
+++ events-manager.php
@@ -7,3 +7,22 @@ function em_ical_cors_headers( ){
 }
 
 add_action ( 'init', 'em_ical_cors_headers', 5 );
+
+function em_add_future_public_scope($scopes) {
+        return array_merge($scopes, [
+                'future-public' => 'Future events excluding private',
+        ]);
+}
+
+add_filter('em_get_scopes', 'em_add_future_public_scope', 5);
+
+function em_add_future_public_to_conditions($args) {
+        if ($args['scope'] === 'future-public') {
+                $args['scope'] = 'future';
+                $args['tag'] = '-private';
+        }
+
+        return $args;
+}
+
+add_filter('em_object_build_sql_conditions_args', 'em_add_future_public_to_conditions', 5);
